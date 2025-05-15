2e33b8346e954c2dd15496ccbc61b007131357de
luisguillermomolero <luisguillermomolero@gmail.com>
2025-05-14 21:43:08 -0500

    commit

--- 04_MODULO_4/04_CUARTA_SEMANA/proyecto/proyecto/src/main/java/com/proyecto/repository/UsuarioRepository.java
+++ 04_MODULO_4/04_CUARTA_SEMANA/proyecto/proyecto/src/main/java/com/proyecto/repository/UsuarioRepository.java
@@ -1,5 +1,10 @@
 package com.proyecto.repository;
 
-public class UsuarioRepository {
+import com.proyecto.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 }
--- 04_MODULO_4/04_CUARTA_SEMANA/proyecto/proyecto/src/main/java/com/proyecto/repository/VentaRepository.java
+++ 04_MODULO_4/04_CUARTA_SEMANA/proyecto/proyecto/src/main/java/com/proyecto/repository/VentaRepository.java
@@ -1,5 +1,10 @@
 package com.proyecto.repository;
 
-public class VentaRepository {
+import com.proyecto.models.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VentaRepository extends JpaRepository<Venta, Long> {
 
 }

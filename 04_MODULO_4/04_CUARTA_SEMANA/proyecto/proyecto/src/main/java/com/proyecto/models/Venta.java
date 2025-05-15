aa1c94eb82a9ddaac197159361154678ac45094e
luisguillermomolero <luisguillermomolero@gmail.com>
2025-05-14 21:30:19 -0500

    commit

--- 04_MODULO_4/04_CUARTA_SEMANA/proyecto/proyecto/src/main/java/com/proyecto/models/Venta.java
+++ 04_MODULO_4/04_CUARTA_SEMANA/proyecto/proyecto/src/main/java/com/proyecto/models/Venta.java
@@ -1,5 +1,47 @@
 package com.proyecto.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "venta")
+
 public class Venta {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "producto_id", nullable = false)
+    private Producto producto;
+
+    @ManyToOne
+    @JoinColumn(name = "vendedor_id", nullable = false)
+    private Usuario usuario;
+
+    @Column(nullable = false)
+    private Integer cantidad;
+
+    @Column(nullable = false)
+    private LocalDate fecha;
+
+    public Venta(Producto producto, Usuario cliente, Usuario vendedor, Integer cantidad, LocalDate fecha) {
+        this.producto = producto;
+        this.usuario = cliente;
+        this.usuario = vendedor;
+        this.cantidad = cantidad;
+        this.fecha = fecha;
+    }
+
+    public String toString() {
+        return "Venta #" + id + " | " + cantidad + " x " + producto.getNombre() + " a " +
+                usuario.getNombre() + " por " + usuario.getNombre() + " el " + fecha;
+    }
 }
--- 04_MODULO_4/04_CUARTA_SEMANA/proyecto/proyecto/src/main/java/com/proyecto/repository/ProductoRepository.java
+++ 04_MODULO_4/04_CUARTA_SEMANA/proyecto/proyecto/src/main/java/com/proyecto/repository/ProductoRepository.java
@@ -1,5 +1,10 @@
 package com.proyecto.repository;
 
-public class ProductoRepository {
+import com.proyecto.models.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
 
 }
